fbfe0aa86054c8e69cb638dbee8d8c9edddceec4
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-11-09 08:18:20 +0000

    Initial structure of Selector.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 import Selector from './Selector';
 import ProductDisplay from './ProductDisplay'
 import SupplierDisplay from './SupplierDisplay'
--- src/Selector.js
+++ src/Selector.js
@@ -4,7 +4,17 @@ import React, { Component } from 'react';
 class Selector extends Component {
     render() {
         return (
-            <h1>selector</h1>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-2">
+                        <button className='btn btn-block sm-2 btn-primary active'>Link 1</button>
+                        <button className='btn btn-block sm-2 btn-primary'>Link 2</button>
+                    </div>
+                    <div className="col">
+                    Content
+                    </div>         
+                </div>
+            </div>            
         );
     }
 }
