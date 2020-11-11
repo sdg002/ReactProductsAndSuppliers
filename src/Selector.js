d8dfa7f291ae04864d61c37151daccb18617a1e9
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-11-11 08:19:54 +0000

    Completed Selector button state management

--- src/Selector.js
+++ src/Selector.js
@@ -2,13 +2,33 @@
 import React, { Component } from 'react';
 
 class Selector extends Component {
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            selection: React.Children.toArray(props.children)[0].props.name
+        }
+
+    }
     render() {
+        /*
+        We are binding dynamically to all the children elements specified under the Selector component
+        For every child, we are creating a Button
+            -Handle OnClick and set the selected state
+            -Use a method to get the className dynamically
+        */
         return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">
-                        <button className='btn btn-block sm-2 btn-primary active'>Link 1</button>
-                        <button className='btn btn-block sm-2 btn-primary'>Link 2</button>
+                        {
+                            React.Children.map(this.props.children, c=>
+                            <button 
+                                name={c.props.name}
+                                onClick={(ev)=>this.onClick(ev)}
+                                className={this.getMenuClassName(c)}>{c.props.name}</button>
+                                )
+                        }
                     </div>
                     <div className="col">
                     Content
@@ -16,6 +36,21 @@ class Selector extends Component {
                 </div>
             </div>            
         );
+    }
+    onClick(ev)
+    {
+        console.log(`got a click ${ev.target.name}`)
+        this.setState({
+            selection:ev.target.name
+        });
+    }
+    getMenuClassName(child)
+    {
+        let activeButtonState='btn-primary active'
+        let inactiveButtonState='btn-secondary'
+        let buttonState= (this.state.selection === child.props.name) ? activeButtonState:inactiveButtonState;
+        let className=`btn btn-block sm-2 ${buttonState}`;
+        return className;
     }
 }
 
