f08bb2ebd5520882610d9656d63b975a6b1b6b6f
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2020-11-12 07:43:52 +0000

    Got the product table and edit item click and back button sequence to work

--- src/ProductDisplay.js
+++ src/ProductDisplay.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component ,useState}  from 'react';
 import {ProductTable} from './ProductTable'
 import { connect } from "react-redux";
+import {ProductEditor} from './ProductEditor'
+//import { Button } from 'bootstrap';
 
 
 /*
@@ -16,20 +18,80 @@ const mapDispatchToProps = {
 
 const connectFunction = connect(mapStateToProps, mapDispatchToProps);
 
+
 class ProductDisplay extends Component {
     constructor(props)
     {
         super(props);
         this.products=this.props.myproducts;
+        //const [uiState, setUIState] = useState(0); //Will not work in Class components - function components only
+        //React Hook "useState" cannot be called in a class component.
+        this.state=
+        {
+            uiState:0,
+            currentItem:null
+        }
     }
-    render() {
+    OnEditItemClick(product)
+    {
+        console.log(`Edit callback name=${product.name}`)
+        this.setState(
+            {
+                uiState:1,
+                currentItem:product
+            }
+        )
+    }
+    OnDeleteItemClick(product)
+    {
+        console.log(`Delete callback name=${product.name}`)
+    }
+    render()
+    {
+        if (this.state.uiState == 0)
+        {
+            return this.renderMainTable();
+        }
+        else if (this.state.uiState ==1)
+        {
+            return this.renderProductEditor();
+        }
+        else
+        {
+            throw 'Invalid ui state'
+        }
+    }
+    renderMainTable() {
         return (
             <div>
                 <h2>Product display - connect the table via table connector - HOC component (read about this). Count of products={this.products.length}</h2> 
                 <hr/>
-                <ProductTable products={this.products}></ProductTable>           
+                <ProductTable 
+                products={this.products}
+                editCallback={(item)=>{this.OnEditItemClick(item)}}
+                deleteCallback={this.OnDeleteItemClick}
+                ></ProductTable>           
             </div>
         );
+    }
+    OnGoBackFromEditing()
+    {
+        this.setState(
+            {
+                uiState:0,
+            }
+        )
+
+    }
+    renderProductEditor()
+    {
+        return (
+        <div>
+            <h2>Edit product:{this.state.currentItem.name}</h2>
+            <button 
+                className='btn-primary active' 
+                onClick={()=>this.OnGoBackFromEditing()}>Back</button>
+        </div>)
     }
 }
 
